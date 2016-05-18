b6413d4ad00bda8a4f115e57ca8b1b7b1cca7748
Mike Slinn <mslinn@micronauticsresearch.com>
2016-05-18 11:02:58 -0700

    -

--- src/main/scala/com/knol/db/repo/BankRepository.scala
+++ src/main/scala/com/knol/db/repo/BankRepository.scala
@@ -3,6 +3,20 @@ package com.knol.db.repo
 import com.knol.db.connection._
 import scala.concurrent.Future
 
+private[repo] trait BankTable { this: DBComponent =>
+  import driver.api._
+
+  private[BankTable] class BankTable(tag: Tag) extends Table[Bank](tag, "bank") {
+    val id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    val name = column[String]("name")
+    def * = (name, id.?) <> (Bank.tupled, Bank.unapply)
+  }
+
+  protected val bankTableQuery = TableQuery[BankTable]
+
+  protected def bankTableAutoInc = bankTableQuery returning bankTableQuery.map(_.id)
+}
+
 trait BankRepository extends BankTable { this: DBComponent =>
   import driver.api._
 
@@ -20,20 +34,6 @@ trait BankRepository extends BankTable { this: DBComponent =>
 
   /** delete bank by id */
   def delete(id: Int): Future[Int] = db.run { bankTableQuery.filter(_.id === id).delete }
-}
-
-private[repo] trait BankTable { this: DBComponent =>
-  import driver.api._
-
-  private[BankTable] class BankTable(tag: Tag) extends Table[Bank](tag, "bank") {
-    val id = column[Int]("id", O.PrimaryKey, O.AutoInc)
-    val name = column[String]("name")
-    def * = (name, id.?) <> (Bank.tupled, Bank.unapply)
-  }
-
-  protected val bankTableQuery = TableQuery[BankTable]
-
-  protected def bankTableAutoInc = bankTableQuery returning bankTableQuery.map(_.id)
 }
 
 object BankRepository extends BankRepository with PostgresDBComponent
