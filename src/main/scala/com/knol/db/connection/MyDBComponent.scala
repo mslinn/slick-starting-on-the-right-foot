73519ada4c07490d464c3648a6578e009a59c059
Mike Slinn <mslinn@micronauticsresearch.com>
2016-05-19 07:49:51 -0700

    Defined SelectedDB as a type alias instead of a trait to remove unnecessary class

--- src/main/scala/com/knol/db/connection/MyDBComponent.scala
+++ src/main/scala/com/knol/db/connection/MyDBComponent.scala
@@ -1,31 +1,34 @@
-package com.knol.db.connection
+package com.knol.db
 
-trait DBComponent {
-  import slick.driver.JdbcProfile
+package connection {
+  trait DBComponent {
+    import slick.driver.JdbcProfile
 
-  val driver: JdbcProfile
+    val driver: JdbcProfile
 
-  import driver.api._
+    import driver.api._
 
-  val db: Database
+    val db: Database
+  }
+
+  trait MySqlDBComponent extends DBComponent {
+    import slick.driver.MySQLDriver.api._
+    import slick.driver.MySQLDriver
+
+    val driver = MySQLDriver
+    val db: Database = Database.forConfig("mysql")
+  }
+
+  trait PostgresDBComponent extends DBComponent {
+    import slick.driver.PostgresDriver.api._
+    import slick.driver.PostgresDriver
+
+    val driver = PostgresDriver
+    val db: Database = Database.forConfig("postgres")
+  }
 }
 
-
-trait MySqlDBComponent extends DBComponent {
-  import slick.driver.MySQLDriver.api._
-  import slick.driver.MySQLDriver
-
-  val driver = MySQLDriver
-  val db: Database = Database.forConfig("mysql")
+package object connection {
+  // Redefine as extending MySqlDBComponent to use MySQL instead; no other changes in app required
+  type SelectedDB = PostgresDBComponent
 }
-
-trait PostgresDBComponent extends DBComponent {
-  import slick.driver.PostgresDriver.api._
-  import slick.driver.PostgresDriver
-
-  val driver = PostgresDriver
-  val db: Database = Database.forConfig("postgres")
-}
-
-// Redefine as extending MySqlDBComponent to use MySQL instead; no other changes in app required
-trait SelectedDB extends PostgresDBComponent
