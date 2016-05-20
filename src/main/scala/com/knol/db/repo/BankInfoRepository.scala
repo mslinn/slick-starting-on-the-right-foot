cfe5e010a56cd547b1bb79b9aaa6efedd4b86551
Mike Slinn <mslinn@micronauticsresearch.com>
2016-05-20 06:41:49 -0700

    Question posted to Google Group

--- src/main/scala/com/knol/db/repo/BankInfoRepository.scala
+++ src/main/scala/com/knol/db/repo/BankInfoRepository.scala
@@ -2,8 +2,13 @@ package com.knol.db.repo
 
 import com.knol.db.connection.{DBComponent, SelectedDB}
 import scala.concurrent.{Await, Future}
+import concurrent.duration.Duration
 
-case class BankInfo(owner: String, branches: Int, bankId: Int, id: Option[Int] = None) {
+trait HasId {
+  def id: Option[Int] = None
+}
+
+case class BankInfo(owner: String, branches: Int, bankId: Int, override val id: Option[Int] = None) extends HasId {
   override def toString = s"""BankInfo for $owner; bankId #$bankId; $branches branches."""
 }
 
@@ -26,43 +31,45 @@ private[repo] trait BankInfoTable extends BankTable { this: DBComponent =>
   def autoInc = tableQuery returning tableQuery.map(_.id)
 }
 
-trait DbAction[T] { this: DBComponent =>
+/** Handles all actions pertaining to HasId or that do not require parameters */
+trait DbAction[T <: HasId] { this: DBComponent =>
   import driver.api._ // defines DBIOAction
 
-  type QueryType <: slick.lifte.TableQuery[Table[T]] // this is wrong! What should it be?
+  type QueryType <: slick.lifted.TableQuery[Table[T]] // this is wrong! What should it be?
   def tableQuery: QueryType
 
-  def run[R](action: DBIOAction[R, NoStream, Nothing]): Future[R] = db.run { action }
+  // Is this defined correctly?
+  @inline def run[R](action: DBIOAction[R, NoStream, Nothing]): Future[R] = db.run { action }
 
-  def deleteById(id: Option[Long]): Unit =
-    for { i <- id } run { tableQuery.filter(_.id === id).delete } // id is unknown
-
-  def findAll: Future[List[T]] = run { tableQuery.to[List].result }
-}
-
-private[repo] trait BankInfoRepositoryLike extends BankInfoTable with DbAction[BankInfo] { this: DBComponent =>
-  import concurrent.duration.Duration
-  import driver.api._
-
-  @inline def createAsync(bankInfo: BankInfo): Future[Int] = run { autoInc += bankInfo }
-  @inline def create(bankInfo: BankInfo): Int = Await.result(createAsync(bankInfo), Duration.Inf)
+  @inline def deleteAsync(id: Int): Future[Int] = run { tableQuery.filter(_.id === id).delete }
+  @inline def delete(id: Int): Int = Await.result(deleteAsync(id), Duration.Inf)
 
   @inline def deleteAllAsync(): Future[Int] = run { tableQuery.delete }
   @inline def deleteAll(): Int = Await.result(deleteAllAsync(), Duration.Inf)
+
+  @inline def getAllAsync: Future[List[BankInfo]] = run { tableQuery.to[List].result }
+  @inline def getAll: List[BankInfo] = Await.result(getAllAsync, Duration.Inf)
+
+  @inline def getByIdAsync(id: Int): Future[Option[BankInfo]] =
+    run { tableQuery.filter(_.id === id).result.headOption }
+
+  @inline def getById(id: Int): Option[BankInfo] = Await.result(getByIdAsync(id), Duration.Inf)
+
+  @inline def deleteById(id: Option[Int]): Unit =
+    for { i <- id } run { tableQuery.filter(_.id === id).delete } // id is unknown
+
+  @inline def findAll: Future[List[T]] = run { tableQuery.to[List].result } // also b0rked
+}
+
+private[repo] trait BankInfoRepositoryLike extends BankInfoTable with DbAction[BankInfo] { this: DBComponent =>
+  import driver.api._
 
   @inline def updateAsync(bankInfo: BankInfo): Future[Int] =
     run { tableQuery.filter(_.id === bankInfo.id.get).update(bankInfo) }
   @inline def update(bankInfo: BankInfo): Int = Await.result(updateAsync(bankInfo), Duration.Inf)
 
-  @inline def getByIdAsync(id: Int): Future[Option[BankInfo]] =
-    run { tableQuery.filter(_.id === id).result.headOption }
-  @inline def getById(id: Int): Option[BankInfo] = Await.result(getByIdAsync(id), Duration.Inf)
-
-  @inline def getAllAsync: Future[List[BankInfo]] = run { tableQuery.to[List].result }
-  @inline def getAll: List[BankInfo] = Await.result(getAllAsync, Duration.Inf)
-
-  @inline def deleteAsync(id: Int): Future[Int] = run { tableQuery.filter(_.id === id).delete }
-  @inline def delete(id: Int): Int = Await.result(deleteAsync(id), Duration.Inf)
+  @inline def createAsync(bankInfo: BankInfo): Future[Int] = run { autoInc += bankInfo }
+  @inline def create(bankInfo: BankInfo): Int = Await.result(createAsync(bankInfo), Duration.Inf)
 
   /** Get bank and info using foreign key relationship */
   @inline def getBankWithInfoAsync: Future[List[(Bank, BankInfo)]] =
